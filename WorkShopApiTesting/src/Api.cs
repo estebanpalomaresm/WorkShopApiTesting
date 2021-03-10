514fe11ba78aaf21f19c6d033bceef6af74c75a8
Esteban Palomares Murillo <epalomaresm@psl.com.co>
2021-03-10 08:50:19 -0500

    FIRST COMMIT: START PROJECT

---
+++ WorkShopApiTesting/src/Api.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+
+namespace WorkShopApiTesting.src
+{
+    public class Api
+    {
+        private string url = "https://httpbin.org";
+        private RestClient client;
+
+        public Api()
+        {
+            client = new RestClient(url);
+        }
+
+        public void GetIp()
+        {
+
+        }
+
+        public IRestResponse Get()
+        {
+            var request = new RestRequest($"/get",Method.GET);
+            return client.Execute(request);
+        }
+        public IRestResponse Delete()
+        {
+            var request = new RestRequest($"/delete",Method.DELETE);
+            return client.Execute(request);
+        }
+        public IRestResponse Patch()
+        {
+            var request = new RestRequest($"/patch", Method.PATCH);
+            return client.Execute(request);
+        }
+        public IRestResponse Post()
+        {
+            var request = new RestRequest($"/post", Method.POST);
+            return client.Execute(request);
+        }
+        public IRestResponse Put()
+        {
+            var request = new RestRequest($"/put", Method.PUT);
+            return client.Execute(request);
+        }
+        public IRestResponse GetSc()
+        {
+            var request = new RestRequest($"/status/200", Method.GET);
+            return client.Execute(request);
+        }
+        public IRestResponse DeleteSc()
+        {
+            var request = new RestRequest($"/status/200", Method.DELETE);
+            return client.Execute(request);
+        }
+        public IRestResponse PatchSc()
+        {
+            var request = new RestRequest($"/status/200", Method.PATCH);
+            return client.Execute(request);
+        }
+        public IRestResponse PostSc()
+        {
+            var request = new RestRequest($"/status/200", Method.POST);
+            return client.Execute(request);
+        }
+        public IRestResponse PutSc()
+        {
+            var request = new RestRequest($"/status/200", Method.PUT);
+            return client.Execute(request);
+        }
+    }
+}
---
+++ WorkShopApiTesting/test/ApiTest.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkShopApiTesting.src;
+using Xunit;
+
+namespace WorkShopApiTesting.test
+{
+    public class ApiTest
+    {
+        [Fact]
+        public void GetTest()
+        {
+            //Arrange
+            var api = new Api();
+
+            //Act
+            var result = api.Get();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void DeleteTest()
+        {
+            //Arrange
+            var api = new Api();
+
+            //Act
+            var result = api.Delete();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void PatchTest()
+        {
+            //Arrange
+            var api = new Api();
+
+            //Act
+            var result = api.Patch();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void PostTest()
+        {
+            //Arrange
+            var api = new Api();
+
+            //Act
+            var result = api.Post();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void PutTest()
+        {
+            //Arrange
+            var api = new Api();
+
+            //Act
+            var result = api.Put();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void GetScTest()
+        {
+            //Arrange
+            var api = new Api();
+
+            //Act
+            var result = api.GetSc();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void DeleteScTest()
+        {
+            //Arrange
+            var api = new Api();
+
+            //Act
+            var resutl = api.DeleteSc();
+
+            //Assert
+            Assert.NotNull(resutl);
+        }
+        [Fact]
+        public void PatchScTest()
+        {
+            //Arrange
+            var api = new Api();
+
+            //Act
+            var result = api.PatchSc();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void PostScTest()
+        {
+            //Arrange
+            var api = new Api();
+
+            //Act
+            var result = api.PostSc();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void PutScTest()
+        {
+            //Arrange
+            var api = new Api();
+
+            //Act
+            var result = api.PutSc();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+    }
+}
