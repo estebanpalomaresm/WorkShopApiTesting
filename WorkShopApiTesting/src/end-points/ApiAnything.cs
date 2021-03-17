d0e944dd76f6a2682a86e8ce2bf75c5695db28c1
PSLCOL\epalomaresm <esteban.palomaresm@perficient.com>
2021-03-17 08:27:36 -0500

    comparison between two json files

--- WorkShopApiTesting/src/end-points/ApiAnything.cs
+++ WorkShopApiTesting/src/end-points/ApiAnything.cs
@@ -21,5 +21,33 @@ namespace WorkShopApiTesting.src.end_points
             request.AddJsonBody(jsonString);
             return client.Execute(request);
         }
+
+        public IRestResponse Delete(string jsonString)
+        {
+            var request = new RestRequest(Method.DELETE);
+            request.AddJsonBody(jsonString);
+            return client.Execute(request);
+        }
+
+        public IRestResponse Get(string jsonString)
+        {
+            var request = new RestRequest(Method.GET);
+            //request.AddJsonBody(jsonString);
+            return client.Execute(request);
+        }
+
+        public IRestResponse Patch(string jsonString)
+        {
+            var request = new RestRequest(Method.PATCH);
+            request.AddJsonBody(jsonString);
+            return client.Execute(request);
+        }
+
+        public IRestResponse Put(string jsonString)
+        {
+            var request = new RestRequest(Method.PUT);
+            request.AddJsonBody(jsonString);
+            return client.Execute(request);
+        }
     }
 }
--- WorkShopApiTesting/test/ApiAnythingTest.cs
+++ WorkShopApiTesting/test/ApiAnythingTest.cs
@@ -1,7 +1,4 @@
 ﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using WorkShopApiTesting.src.end_points;
 using WorkShopApiTesting.src.utils;
 using Xunit;
@@ -10,20 +7,89 @@ namespace WorkShopApiTesting.test
 {
     public class ApiAnythingTest
     {
-        [Fact]
-        public void PostTest()
+        [Theory]
+        [InlineData("user_01.json")]
+        [InlineData("user_02.json")]
+        public void PostTest(string jsonString)
         {
             //Arrange
             ApiAnything api = new ApiAnything();
-            var jsonString = TestFileLoader.LoadFromFile("user_01.json");
+            var jsonFile = TestFileLoader.LoadFromFile(jsonString);
 
             //Act
-            var result = api.Post(jsonString);
+            var result = api.Post(jsonFile);
+            var jsonResult = JObject.Parse(result.Content);
 
             //Assert
-            var jsonResult = JObject.Parse(result.Content);
-            Assert.Equal(jsonResult["json"].ToString(),jsonString.ToString());
+            Assert.True(JToken.DeepEquals(jsonResult["data"],jsonFile));
         }
 
+        [Theory]
+        [InlineData("user_01.json")]
+        [InlineData("user_02.json")]
+        public void DeleteTest(string jsonString)
+        {
+            //Arrange
+            ApiAnything api = new ApiAnything();
+            var jsonFile = TestFileLoader.LoadFromFile(jsonString);
+
+            //Act
+            var result = api.Delete(jsonFile);
+            var jsonResult = JObject.Parse(result.Content);
+
+            //Assert
+            Assert.True(JToken.DeepEquals(jsonResult["data"], jsonFile));
+        }
+
+        [Theory]
+        [InlineData("user_01.json")]
+        [InlineData("user_02.json")]
+        public void PutTest(string jsonString)
+        {
+            //Arrange
+            ApiAnything api = new ApiAnything();
+            var jsonFile = TestFileLoader.LoadFromFile(jsonString);
+
+            //Act
+            var result = api.Put(jsonFile);
+            var jsonResult = JObject.Parse(result.Content);
+
+            //Assert
+            Assert.True(JToken.DeepEquals(jsonFile, jsonResult["json"]));
+        }
+
+        [Theory]
+        [InlineData("user_01.json")]
+        [InlineData("user_02.json")]
+        public void PatchTest(string jsonString)
+        {
+            //Arrange
+            ApiAnything api = new ApiAnything();
+            var jsonFile = TestFileLoader.LoadFromFile(jsonString);
+
+            //Act
+            var result = api.Patch(jsonFile);
+            var jsonResult = JObject.Parse(result.Content);
+
+            //Assert
+            Assert.True(JToken.DeepEquals(jsonFile, jsonResult["data"]));
+        }
+
+        [Theory]
+        [InlineData("user_01.json")]
+        [InlineData("user_02.json")]
+        public void GetTest(string jsonString)
+        {
+            //Arrange
+            ApiAnything api = new ApiAnything();
+            var jsonFile = TestFileLoader.LoadFromFile(jsonString);
+
+            //Act
+            var result = api.Get(jsonFile);
+            var jsonResult = JObject.Parse(result.Content);
+
+            //Assert
+            Assert.NotNull(jsonResult["json"]);
+        }
     }
 }
